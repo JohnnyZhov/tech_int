6a10cf143b42bb7783214fb37eee302af6303c77
IvanZhovnych <izhovnych@optimiz.ca>
2024-03-28 18:21:50 -0500

    First commit

---
+++ 11. Find middle node.js
@@ -0,0 +1,135 @@
+class Node {
+    constructor(value){
+        this.value = value;
+        this.next = null;
+    }
+}
+ 
+class LinkedList {
+    constructor(value) {
+        const newNode = new Node(value);
+        this.head = newNode;
+        this.tail = this.head;
+    }
+
+    printList() {
+        let temp = this.head;
+        while (temp !== null) {
+            console.log(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    getHead() {
+        if (this.head === null) {
+            console.log("Head: null");
+        } else {
+            console.log("Head: " + this.head.value);
+        }
+    }
+
+    getTail() {
+        if (this.tail === null) {
+            console.log("Tail: null");
+        } else {
+            console.log("Tail: " + this.tail.value);
+        }
+    }
+
+    makeEmpty() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+ 
+    push(value) {
+        const newNode = new Node(value);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+    }
+    
+    getNode(index){
+        if(index < 0 ) return undefined
+        let temp = this.head
+        
+        for(let i = 0; i < index; i++){
+            temp = temp.next
+        }
+        return temp
+    }
+    
+    findMiddleNode(){
+        let middleNodeIndex
+        let temp = this.head
+        let count = 0
+        
+        while(temp){
+            temp = temp.next
+            count++
+        }
+        
+        console.log(`\ncount: ${count}`)
+        
+        if(count == 0) return null
+        
+        if(count % 2 === 0){
+            middleNodeIndex = count / 2 + 1
+            return this.getNode(middleNodeIndex - 1)
+        }else{
+            middleNodeIndex = (count - 1) / 2
+            return this.getNode(middleNodeIndex)
+        }
+    }
+}
+
+let myLinkedList = new LinkedList(1);
+myLinkedList.push(2);
+myLinkedList.push(3);
+myLinkedList.push(4);
+myLinkedList.push(5);
+
+console.log("Original list:");
+myLinkedList.printList();
+
+const middleNode = myLinkedList.findMiddleNode();
+console.log(`\nMiddle node value: ${middleNode.value}`);
+
+// Create a new list with an even number of elements
+let myLinkedList2 = new LinkedList(1);
+myLinkedList2.push(2);
+myLinkedList2.push(3);
+myLinkedList2.push(4);
+myLinkedList2.push(5);
+myLinkedList2.push(6);
+
+console.log("\nOriginal list 2:");
+myLinkedList2.printList();
+
+const middleNode2 = myLinkedList2.findMiddleNode();
+console.log(`\nMiddle node value of list 2: ${middleNode2.value}`);
+
+
+/*
+    EXPECTED OUTPUT:
+    ----------------
+    Original list:
+    1
+    2
+    3
+    4
+    5
+    Middle node value: 3
+    Original list 2:
+    1
+    2
+    3
+    4
+    5
+    6
+    Middle node value of list 2: 4
+*/
